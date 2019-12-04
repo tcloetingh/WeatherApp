0f565372694f7778cbadc3f70db1071e6b5542ba
Tyler Cloetingh <tylercloetingh@Tylers-MacBook-Air.local>
2019-12-04 18:05:31 -0500

    geo location and css

---
+++ src/run.js
@@ -0,0 +1,12 @@
+const seasonConfig = {
+  summer: {
+    text: 'lets hit the beach!',
+    whatIcon: 'sun'
+  }, 
+  winter: {
+    text: 'burr its cold',
+    whatIcon: 'snowflake'
+  }
+};
+
+console.log(seasonConfig);
